4cdf114bd7a3163a36667f2fc778b09f1ca14eea
olme97 <olme97@hotmail.com>
2023-02-07 14:27:46 +0100

    Carrito de compra

---
+++ src/main/java/es/codeurjc/web/model/CarritodeCompra.java
@@ -0,0 +1,60 @@
+package es.codeurjc.web.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+//@Entity
+public class CarritodeCompra {
+    
+    //@Id
+	//@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
+	
+	//@OneToOne
+	private Usuario usuario;
+	
+	//@OneToMany
+	private  List<Producto> listaProductos; 
+
+
+	public CarritodeCompra() {
+		
+	}
+	//POR DEFECTO CUANDO ENCIENDES LA PAGINA
+	public CarritodeCompra(Usuario u){
+		this.usuario=u;
+		this.listaProductos = new ArrayList<>();
+	}
+	//FILTRADO POR USUARIO METIENDO UN PRODUCTO EN LA LISTA DEL CARRITO
+	public CarritodeCompra(Usuario u,Producto x){
+		this.usuario=u;
+		this.listaProductos = new ArrayList<>();
+		this.listaProductos.add(x);
+	}
+	
+	
+	
+	public Usuario getUsuario() {
+		return usuario;
+	}
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
+	
+
+	public  List<Producto> getListaProductos() {
+		return listaProductos;
+	}
+
+	
+	public void setListaProductos(List<Producto> listaProductos) {
+		this.listaProductos = listaProductos;
+	}
+	
+	@Override
+	public String toString() {
+		return "CarritoCompra [Usuario=" + usuario + ", Producto=" + listaProductos + "]";
+	}
+
+	
+}
