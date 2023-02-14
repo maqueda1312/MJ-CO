3f54efbb979b6eb9b2b71a4405532e86aaf3e520
maqueda1312 <ivanmaquedaguede@gmail.com>
2023-02-14 14:44:02 +0100

    .

--- src/main/java/es/codeurjc/web/controller/PrincipalController.java
+++ src/main/java/es/codeurjc/web/controller/PrincipalController.java
@@ -1,13 +1,19 @@
 package es.codeurjc.web.controller;
 
 import es.codeurjc.web.model.Producto;
+import org.hibernate.engine.jdbc.BlobProxy;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import es.codeurjc.web.service.ProductoService;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.List;
 import java.util.Optional;
 
 public class PrincipalController {
@@ -35,4 +41,5 @@ public class PrincipalController {
         }
 
     }
+
 }
--- src/main/java/es/codeurjc/web/controller/ProductoController.java
+++ src/main/java/es/codeurjc/web/controller/ProductoController.java
@@ -1,5 +1,19 @@
 package es.codeurjc.web.controller;
 
+import org.hibernate.engine.jdbc.BlobProxy;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Optional;
+
 public class ProductoController {
-    
+
+
 }
