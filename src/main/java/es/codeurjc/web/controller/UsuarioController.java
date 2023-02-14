e78899b3c6e214c28e34bddaae1386ece14a08d9
olme97 <olme97@hotmail.com>
2023-02-14 14:06:03 +0100

    ..

--- src/main/java/es/codeurjc/web/controller/UsuarioController.java
+++ src/main/java/es/codeurjc/web/controller/UsuarioController.java
@@ -10,31 +10,7 @@ import es.codeurjc.web.model.Usuario;
 
 @Controller
 public class UsuarioController {
-	
-	//@Autowired
-	//private PackRepository packs;
-	
-	//private ComentarioRepository coment;
-	//@Autowired
-	//private UserRepository users;
-	
-	//private PasswordEncoder encoder;
-	
-	
-	
-	@PostMapping("/nuevousuario")
-	public String agregarUsuario(Model model, Usuario usuario) {
-		System.out.println(usuario.getNombre());
-		System.out.println(usuario.getContrasena());
-		//usuario.save(new Usuario(usuario.getNombre(), usuario.getContrasena(), usuario.getMail(),  "ROLE_USER"));
-		model.addAttribute("usuario", usuario);
-		return "comprobacion";	
-	}
-	
-	@GetMapping("/agregarusu")
-	public String agregarUsu(Model model) {
-		return "agregarusu";
-	}
+
 }
 
 
