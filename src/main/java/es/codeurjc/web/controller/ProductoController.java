b86486dd1ed94c4442116bf9b793516908f17f0b
olme97 <olme97@hotmail.com>
2023-02-21 14:11:46 +0100

    formulario de nuevos productos

--- src/main/java/es/codeurjc/web/controller/ProductoController.java
+++ src/main/java/es/codeurjc/web/controller/ProductoController.java
@@ -1,6 +1,8 @@
 package es.codeurjc.web.controller;
 
 import org.hibernate.engine.jdbc.BlobProxy;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -8,12 +10,35 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
+import es.codeurjc.web.model.Producto;
+import es.codeurjc.web.service.ProductoService;
+
 import java.io.IOException;
 import java.sql.SQLException;
 import java.util.List;
 import java.util.Optional;
 
+@Controller
 public class ProductoController {
 
+    @Autowired
+    private ProductoService productoService;
+
+    @GetMapping("/nuevoProducto")
+	public String newBook(Model model) {
+
+	    return "nuevoProducto";
+	}
+
+
+    @PostMapping("/nuevoProducto")
+	public String nuevoProdcutoProceso(Model model, Producto producto) throws IOException {
+
+		productoService.save(producto);
+
+		model.addAttribute("productoId", producto.getId());
+
+		return "redirect:/producto/" + producto.getId();
+	}
 
 }
