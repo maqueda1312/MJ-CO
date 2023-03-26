dab96cb10c792cb9c2d2b800e263ba2ba1fb72d7
olme97 <olme97@hotmail.com>
2023-03-27 01:42:16 +0200

    MODIFICACION DE RUNDB PARA INSTRUCCIONES

--- rundb.bat
+++ rundb.bat
@@ -1,3 +1,11 @@
-docker run --rm -e MYSQL_ROOT_PASSWORD=password -e MYSQL_DATABASE=productos -p 3306:3306 -d mysql:8.0
+@REM LANZAR DOCKER PARA LA DB
+@REM docker run --rm -e MYSQL_ROOT_PASSWORD=password -e MYSQL_DATABASE=productos -p 3306:3306 -d mysql:8.0
 
-@REM java -jar target\helloworld-vscode-0.0.1-SNAPSHOT.jar --server.port=3000 --email.user=mjcourjc@gmail.com --email.password="ytrvlhjhgordlcmn"
+
+
+@REM LANZAR SERVICIO INTERNO
+@REM java -jar target\helloworld-vscode-0.0.1-SNAPSHOT.jar --server.port=3000 --email.user=mjcoapp@outlook.es --email.password=pepitogatO 
+
+
+@REM LANZAR WEB SERVICE
+@REM java -jar target\web_ejer1-0.1.0-SNAPSHOT.jar
